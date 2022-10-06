f3b24f7ce37c1998effc7fd5c1df28b01d5dfa1a
s.sugiyama <s.sugiyama0811@gmail.com>
2022-10-06 02:18:39 +0000

    typescriptに変更

---
+++ hardhat.config.ts
@@ -0,0 +1,15 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true
+      }
+    }
+  }
+};
+
+export default config;
