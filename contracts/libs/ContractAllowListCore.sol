703ba6a26a5f485405e7e92689f9fc0288119535
henma-dev <e_pinanko@yahoo.co.jp>
2022-10-07 18:29:28 +0900

    ・リスト一覧取得（AccessControlEnumerableで実現） ・以下のロールとした 　・オーナー：DEFAULT_ADMIN_ROLE 　・追加＋削除＋ロール管理：ADMIN 　・追加のみ：SETTER

--- contracts/libs/ContractAllowListCore.sol
+++ contracts/libs/ContractAllowListCore.sol
@@ -1,46 +1,58 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 
-contract ContractAllowListCore is Ownable, AccessControl{
+contract ContractAllowListCore is AccessControlEnumerable{
 
     constructor() {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _setupRole(ADMINISTRATOR, msg.sender);
+        _setupRole(ADMIN, msg.sender);
         //addContractAllowList( 0x1E0049783F008A0085193E00003D00cd54003c71);//opensea goerli test network
     }
 
-    bytes32 public constant ADMINISTRATOR = keccak256("ADMINISTRATOR");
-    bytes32 public constant CONTRACT_ALLOW_LIST = keccak256("CONTRACT_ALLOW_LIST");
-
-    // onlyOwner
-    function setAdminRole(address[] memory admins) external onlyOwner{
-        for (uint256 i = 0; i < admins.length; i++) {
-            _setupRole(ADMINISTRATOR, admins[i]);
-        }
-    }
-
-    function revokeAdminRole(address[] memory admins) external onlyOwner{
-        for (uint256 i = 0; i < admins.length; i++) {
-            _revokeRole(ADMINISTRATOR, admins[i]);
-        }
-    }
+    bytes32 public constant ADMIN = keccak256("ADMIN");
+    bytes32 public constant SETTER = keccak256("SETTER");
+    bytes32 public constant CONTRACT_ALLOW_LIST = keccak256("CONTRACT_ALLOW_LIST");   
 
     // modifier
     modifier onlyAdmin() {
-        require(hasRole(ADMINISTRATOR, msg.sender), "Caller is not a administrator.");
+        require(hasRole(ADMIN, msg.sender), "Caller is not a administrator.");
+        _;
+    }
+    modifier  onlySetter() {
+        require(hasRole(ADMIN, msg.sender) || hasRole(SETTER, msg.sender), "Caller is not a setter.");
         _;
     }
 
     // external
-    function addContractAllowList(address _address) external onlyAdmin{
-        _grantRole(CONTRACT_ALLOW_LIST, _address);
+    function setAdminRole(address[] memory admins) external onlyAdmin{
+        for (uint256 i = 0; i < admins.length; i++) {
+            _setupRole(ADMIN, admins[i]);
+        }
+    }
+
+    function revokeAdminRole(address[] memory admins) external onlyAdmin{
+        for (uint256 i = 0; i < admins.length; i++) {
+            _revokeRole(ADMIN, admins[i]);
+        }
+    }
+
+    function addContractAllowList(address _address) external onlySetter{
+        _setupRole(CONTRACT_ALLOW_LIST, _address);
     }
 
     function deleteContractAllowList(address _address) external onlyAdmin{
         _revokeRole(CONTRACT_ALLOW_LIST, _address);
+    }
+
+    function getContractAllowList() external view returns(address[] memory){
+        uint256 ListCnt =  getRoleMemberCount(CONTRACT_ALLOW_LIST);
+        address[] memory allowaddress = new address[](ListCnt);
+        for(uint256 i; i < ListCnt; i++){
+            allowaddress[i] = getRoleMember(CONTRACT_ALLOW_LIST,i);
+        }
+        return allowaddress;
     }
 
     function checkContractAllowList(address _address) external view returns(bool){
