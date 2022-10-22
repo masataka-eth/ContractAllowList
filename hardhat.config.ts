f14aa239180f8d85edb53050e67e8b68823c1680
s.sugiyama <s.sugiyama0811@gmail.com>
2022-10-22 05:20:46 +0000

    余分な設定を削除

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -44,10 +44,4 @@ if(process.env.ALCHEMY_KEY){
   }
 }
 
-if(process.env.ALCHEMY_KEY){
-  config.networks!.goerli = {
-    url: "https://eth-goerli.g.alchemy.com/v2/" + process.env.ALCHEMY_KEY,
-    accounts: [`${process.env.PRIVATE_KEY}`],
-  }
-}
 export default config;
